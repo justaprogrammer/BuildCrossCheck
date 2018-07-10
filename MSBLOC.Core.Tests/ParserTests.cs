2712f1949b66ae2eb1990ab69a65a297b1d64d05
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-07-10 15:12:28 -0400

    Checking the build log

---
+++ MSBLOC.Core.Tests/ParserTests.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace MSBLOC.Core.Tests
+{
+    [TestFixture]
+    public class ParserTests
+    {
+        private static readonly ILogger<ParserTests> logger = TestLogger.Create<ParserTests>();
+
+        private static string GetResourcePath(string file)
+        {
+            return Path.Combine(TestContext.CurrentContext.TestDirectory, "Resources", file);
+        }
+
+        [Test]
+        public void ParseBinaryLogTest()
+        {
+            var resourcePath = GetResourcePath("testconsoleapp1-warning.binlog");
+            FileAssert.Exists(resourcePath);
+
+            var parser = new Parser(TestLogger.Create<Parser>());
+            parser.Parse(resourcePath);
+        }
+    }
+}
--- MSBLOC.Core/Parser.cs
+++ MSBLOC.Core/Parser.cs
@@ -1,6 +1,37 @@
-﻿namespace MSBLOC.Core
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Logging;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace MSBLOC.Core
 {
     public class Parser
     {
+        private ILogger<Parser> Logger { get; }
+
+        public Parser(ILogger<Parser> logger = null)
+        {
+            Logger = logger ?? new NullLogger<Parser>();
+        }
+
+        public void Parse(string resourcePath)
+        {
+            var binLogReader = new BinaryLogReplayEventSource();
+
+            foreach (var record in binLogReader.ReadRecords(resourcePath))
+            {
+                var buildEventArgs = record.Args;
+                if (buildEventArgs is BuildWarningEventArgs buildWarning)
+                {
+                    Logger.LogInformation($"{buildWarning.File} {buildWarning.LineNumber}:{buildWarning.ColumnNumber} {buildWarning.Message}");
+                }
+
+                if (buildEventArgs is BuildErrorEventArgs buildError)
+                {
+                    Logger.LogInformation($"{buildError.File} {buildError.LineNumber}:{buildError.ColumnNumber} {buildError.Message}");
+                }
+            }
+        }
     }
 }
