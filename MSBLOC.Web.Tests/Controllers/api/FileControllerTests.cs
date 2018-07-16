0b1247fde9dd82a45d69d4e00ad65597e035497f
Ben Grabkowitz <bgrabkowitz@gmail.com>
2018-07-16 18:51:18 -0400

    Adding multi file unit test

--- MSBLOC.Web.Tests/Controllers/api/FileControllerTests.cs
+++ MSBLOC.Web.Tests/Controllers/api/FileControllerTests.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Bogus;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Internal;
@@ -35,38 +36,71 @@ namespace MSBLOC.Web.Tests.Controllers.api
             _testOutputHelper = testOutputHelper;
         }
 
-        private static string GetResourcePath(string file)
-        {
-            var codeBaseUrl = new Uri(Assembly.GetExecutingAssembly().CodeBase);
-            var codeBasePath = Uri.UnescapeDataString(codeBaseUrl.AbsolutePath);
-            var dirPath = Path.GetDirectoryName(codeBasePath);
-            dirPath.Should().NotBeNull();
-            return Path.Combine(dirPath, "Resources", file);
-        }
-
         [Fact]
-        public async Task UploadTest()
+        public async Task UploadFileTest()
         {
-            var fileContents = "This is some dummy file contents";
+            var fileContent = "This is some dummy file contents";
 
             var fileService = Substitute.For<ITempFileService>();
-            fileService.CreateFromStreamAsync(Arg.Any<Stream>()).ReturnsForAnyArgs("dummyFileName.txt");
-            var fileController = new FileController(TestLogger.Create<FileController>(_testOutputHelper), fileService);
-            fileController.ControllerContext = await RequestWithFile(fileContents);
+
+            string receiveFileContent = null;
+
+            fileService.CreateFromStreamAsync(Arg.Do<Stream>(stream =>
+            {
+                receiveFileContent = new StreamReader(stream, Encoding.UTF8).ReadToEnd();
+            })).ReturnsForAnyArgs("dummyFileName.txt");
+
+            var fileController = new FileController(TestLogger.Create<FileController>(_testOutputHelper), fileService)
+            {
+                ControllerContext = await RequestWithFiles(fileContent)
+            };
 
             await fileController.Upload();
 
             await fileService.Received(1).CreateFromStreamAsync(Arg.Any<Stream>());
+
+            receiveFileContent.Should().Be(fileContent);
         }
 
-        private async Task<ControllerContext> RequestWithFile(string fileContents)
+        [Fact]
+        public async Task UploadFilesTest()
         {
-            using (var formDataContent = new MultipartFormDataContent("---9908908098"))
-            {
-                formDataContent.Add(new ByteArrayContent(Encoding.UTF8.GetBytes(fileContents)), "files", "dummy.txt");
+            var fileContents = new Faker<string>().CustomInstantiator(f => f.Lorem.Paragraphs(3, 10)).Generate(4).ToArray();
 
+            var fileService = Substitute.For<ITempFileService>();
+
+            var receivedFileContents = new List<string>();
+
+            fileService.CreateFromStreamAsync(Arg.Do<Stream>(stream =>
+            {
+                receivedFileContents.Add(new StreamReader(stream, Encoding.UTF8).ReadToEnd());
+            })).ReturnsForAnyArgs("dummyFileName.txt");
+
+            var fileController = new FileController(TestLogger.Create<FileController>(_testOutputHelper), fileService)
+            {
+                ControllerContext = await RequestWithFiles(fileContents)
+            };
+
+            await fileController.Upload();
+
+            await fileService.Received(fileContents.Length).CreateFromStreamAsync(Arg.Any<Stream>());
+
+            receivedFileContents.Should().BeEquivalentTo(fileContents);
+        }
+
+        private static async Task<ControllerContext> RequestWithFiles(params string[] fileContents)
+        {
+            var boundary = "---9908908098";
+
+            using (var formDataContent = new MultipartFormDataContent(boundary))
+            {
+                for (var x = 0; x < fileContents.Length; x++)
+                {
+                    formDataContent.Add(new ByteArrayContent(Encoding.UTF8.GetBytes(fileContents[x])), "files", $"dummy.{x}.txt");
+                }
+                
                 var httpContext = new DefaultHttpContext();
-                httpContext.Request.Headers.Add("Content-Type", "multipart/form-data; boundary=---9908908098");
+                httpContext.Request.Headers.Add("Content-Type", $"multipart/form-data; boundary={boundary}");
 
                 httpContext.Request.Body = new MemoryStream(await formDataContent.ReadAsByteArrayAsync());
                 var actionContext = new ActionContext(httpContext, new RouteData(), new ControllerActionDescriptor());
