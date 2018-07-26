92bd0cfb0ecc19ca6aed5b96fe49eb91385b9b8c
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-07-26 16:42:36 -0400

    Optimizing imports

--- MSBLOC.Core/Services/CheckRunSubmitter.cs
+++ MSBLOC.Core/Services/CheckRunSubmitter.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net.NetworkInformation;
 using System.Threading.Tasks;
-using JetBrains.Annotations;
-using Microsoft.Build.Framework;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using MSBLOC.Core.Interfaces;
