75d8498ddb92135dcf6af159a262883f178495dc
Ben Grabkowitz <bgrabkowitz@gmail.com>
2018-07-30 21:00:31 -0400

    Adding doc comments

--- MSBLOC.Web/Interfaces/IMSBLOCService.cs
+++ MSBLOC.Web/Interfaces/IMSBLOCService.cs
@@ -6,6 +6,11 @@ namespace MSBLOC.Web.Interfaces
 {
     public interface IMSBLOCService
     {
+        /// <summary>
+        /// Submits the provided SubmissionData to GitHub
+        /// </summary>
+        /// <param name="submissionData">The submission data.</param>
+        /// <returns>An Octokit CheckRun object</returns>
         Task<CheckRun> SubmitAsync(SubmissionData submissionData);
     }
 }
