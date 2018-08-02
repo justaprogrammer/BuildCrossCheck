48e646e64a333d0db60897eaae34160fa2f3bdf9
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-08-01 20:49:37 -0400

    Fixing argument order

--- MSBLOC.Posh/MSBuildLogOctokitChecker.psm1
+++ MSBLOC.Posh/MSBuildLogOctokitChecker.psm1
@@ -91,7 +91,7 @@ function  Send-MsbuildLogAppveyor {
     $CloneRoot = $env:APPVEYOR_BUILD_FOLDER
     $HeadCommit = $env:APPVEYOR_REPO_COMMIT
 
-    Send-MsbuildLog $Path $CloneRoot $RepoOwner $RepoName $HeadCommit
+    Send-MsbuildLog $Path $RepoOwner $RepoName $CloneRoot $HeadCommit
 }
 
 # Export only the functions using PowerShell standard verb-noun naming.
