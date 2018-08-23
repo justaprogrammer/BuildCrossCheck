e7e9026c19a703433e1819e660409ea9b534e027
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-08-23 19:54:17 -0400

    Adding parameters to doc comments

--- MSBLOC.Core/Interfaces/IGitHubAppClientFactory.cs
+++ MSBLOC.Core/Interfaces/IGitHubAppClientFactory.cs
@@ -12,24 +12,30 @@ namespace MSBLOC.Core.Interfaces
         /// <summary>
         /// Create a IGitHubClient configured for GitHub App Installation Authentication.
         /// </summary>
+        /// <param name="tokenGenerator">A token generator configured for the GitHub App.</param>
+        /// <param name="login">The login to authenticate.</param>
         /// <returns>A client</returns>
         Task<IGitHubClient> CreateAppClientForLoginAsync(ITokenGenerator tokenGenerator, string login);
 
         /// <summary>
         /// Create a IGitHubGraphQLClient configured for GitHub App Installation Authentication.
         /// </summary>
+        /// <param name="tokenGenerator">A token generator configured for the GitHub App.</param>
+        /// <param name="login">The login to authenticate.</param>
         /// <returns>A graphql client</returns>
         Task<IGitHubGraphQLClient> CreateAppGraphQLClientForLoginAsync(ITokenGenerator tokenGenerator, string login);
 
         /// <summary>
         /// Create a IGitHubClient configured for GitHub App Authentication.
         /// </summary>
+        /// <param name="tokenGenerator">A token generator configured for the GitHub App.</param>
         /// <returns>A client</returns>
         IGitHubClient CreateAppClient(ITokenGenerator tokenGenerator);
 
         /// <summary>
         /// Create a IGitHubGraphQLClient configured for GitHub App Authentication.
         /// </summary>
+        /// <param name="tokenGenerator">A token generator configured for the GitHub App.</param>
         /// <returns>A graphql client</returns>
         IGitHubGraphQLClient CreateAppGraphQLClient(ITokenGenerator tokenGenerator);
     }
