19e533faa4d97c2e84bb3b6a5842df31bc68e593
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-09-14 08:13:05 -0400

    Starting to add a project to transform the binlog file

---
+++ MSBLOC.MSBuildLog.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MSBLOC.MSBuildLog.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
