548640ae32915313a3e4b3ba2df65442692c6574
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-09-23 11:52:32 -0400

    Resources designer fix

--- BCC.Core.Tests/Resources.Designer.cs
+++ BCC.Core.Tests/Resources.Designer.cs
@@ -9,6 +9,9 @@
 //------------------------------------------------------------------------------
 
 namespace BCC.Core.Tests {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -36,7 +39,7 @@ namespace BCC.Core.Tests {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager(".Core.Tests.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BCC.Core.Tests.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
