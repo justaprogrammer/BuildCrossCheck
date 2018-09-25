b2d147ca7885be9ebab1bb035fbfec899ea9a468
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-09-25 15:01:34 -0400

    Is this how I'm supposed to do it?

--- Cake.BCC/BCCMSBuildLogTool.cs
+++ Cake.BCC/BCCMSBuildLogTool.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Cake.Common.Tools.DotNetCore;
 using Cake.Core;
 using Cake.Core.Annotations;
 using Cake.Core.IO;
@@ -9,23 +10,31 @@ namespace Cake.BCC
 {
     public class BCCMSBuildLogTool : Tool<BCCMSBuildLogToolSettings>
     {
+        private readonly IFileSystem _fileSystem;
         private readonly ICakeEnvironment _environment;
 
         public BCCMSBuildLogTool(IFileSystem fileSystem, ICakeEnvironment environment, IProcessRunner runner, IToolLocator tools)
             : base(fileSystem, environment, runner, tools)
         {
+            _fileSystem = fileSystem;
             _environment = environment;
         }
 
-        public void BCCMSBuildLog(BCCMSBuildLogToolSettings settings)
+        public void BCCMSBuildLog(ICakeContext context, string version, BCCMSBuildLogToolSettings settings)
         {
-            var arguments = new ProcessArgumentBuilder();
+            var programDllPath = _fileSystem.GetDirectory(_environment.GetEnvironmentVariable("USERPROFILE"))
+                .Path.CombineWithFilePath(
+                    $".nuget\\packages\\bcc-submission\\{version}\\tools\\netcoreapp2.1\\BCC.Submission.dll"
+                )
+                .MakeAbsolute(_environment).FullPath;
 
-//            var filePath = FilePath.FromString($"bin\\{settings.Configuration}\\{settings.Framework}\\{project.Name}.dll");
-//            var fullPath = project.Path.GetDirectory().CombineWithFilePath(filePath).MakeAbsolute(_environment);
-//            arguments.Append($"\"{fullPath}\" --target \"dotnet\" --targetargs \"test -c {settings.Configuration} {project.Path.FullPath} --no-build\" --format opencover --output \"{settings.Output}\"");
+            context.DotNetCoreExecute(programDllPath);
 
-            Run(settings, arguments);
+            //            var filePath = FilePath.FromString($"bin\\{settings.Configuration}\\{settings.Framework}\\{project.Name}.dll");
+            //            var fullPath = project.Path.GetDirectory().CombineWithFilePath(filePath).MakeAbsolute(_environment);
+            //            arguments.Append($"\"{fullPath}\" --target \"dotnet\" --targetargs \"test -c {settings.Configuration} {project.Path.FullPath} --no-build\" --format opencover --output \"{settings.Output}\"");
+
+            //            Run(settings, arguments);
         }
 
         protected override string GetToolName()
@@ -41,12 +50,16 @@ namespace Cake.BCC
 
     public class BCCMSBuildLogToolSettings : ToolSettings
     {
+        public string InputPath { get; set; }
+        public string OutputPath { get; set; }
+        public string ClonePath { get; set; }
     }
 
     public static class CoverletAliases
     {
         [CakeMethodAlias]
-        public static void BCCMSBuildLog(this ICakeContext context, BCCMSBuildLogToolSettings settings = null)
+        public static void BCCMSBuildLog(this ICakeContext context, string version, 
+            BCCMSBuildLogToolSettings settings = null)
         {
             if (context == null)
                 throw new ArgumentNullException(nameof(context));
@@ -54,7 +67,8 @@ namespace Cake.BCC
             if (settings == null)
                 throw new ArgumentNullException(nameof(settings));
 
-            new BCCMSBuildLogTool(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools).BCCMSBuildLog(settings);
+            new BCCMSBuildLogTool(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools)
+                .BCCMSBuildLog(context, version, settings);
         }
     }
 }
--- Cake.BCC/BCCSubmissionTool.cs
+++ Cake.BCC/BCCSubmissionTool.cs
@@ -1,31 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using Cake.Common.Tools.DotNetCore;
 using Cake.Core;
 using Cake.Core.Annotations;
 using Cake.Core.IO;
 using Cake.Core.Tooling;
+using Cake.Frosting;
 
 namespace Cake.BCC
 {
     public class BCCSubmissionTool : Tool<BCCSubmissionToolSettings>
     {
+        private readonly IFileSystem _fileSystem;
         private readonly ICakeEnvironment _environment;
 
         public BCCSubmissionTool(IFileSystem fileSystem, ICakeEnvironment environment, IProcessRunner runner, IToolLocator tools)
             : base(fileSystem, environment, runner, tools)
         {
+            _fileSystem = fileSystem;
             _environment = environment;
         }
 
-        public void BCCMSBuildLog(BCCSubmissionToolSettings settings)
+        public void BCCMSBuildLog(ICakeContext context, string version, BCCSubmissionToolSettings settings)
         {
-            var arguments = new ProcessArgumentBuilder();
+            var programDllPath = _fileSystem.GetDirectory(_environment.GetEnvironmentVariable("USERPROFILE"))
+                .Path.CombineWithFilePath(
+                    $".nuget\\packages\\bcc-msbuildlog\\{version}\\tools\\netcoreapp2.1\\BCC.MSBuildLog.dll"
+                )
+                .MakeAbsolute(_environment).FullPath;
 
-//            var filePath = FilePath.FromString($"bin\\{settings.Configuration}\\{settings.Framework}\\{project.Name}.dll");
-//            var fullPath = project.Path.GetDirectory().CombineWithFilePath(filePath).MakeAbsolute(_environment);
-//            arguments.Append($"\"{fullPath}\" --target \"dotnet\" --targetargs \"test -c {settings.Configuration} {project.Path.FullPath} --no-build\" --format opencover --output \"{settings.Output}\"");
+            context.DotNetCoreExecute(programDllPath);
 
-            Run(settings, arguments);
+            // var arguments = new ProcessArgumentBuilder();
+//            context.Directory(context.EnvironmentVariable("USERPROFILE"))
+//                .Path.CombineWithFilePath(".nuget\\packages\\msbuild.structuredlogger\\2.0.11\\lib\\netstandard2.0\\StructuredLogger.dll")
+//                .MakeAbsolute(context.Environment)
+//                .FullPath;
+
+            //            var filePath = FilePath.FromString($"bin\\{settings.Configuration}\\{settings.Framework}\\{project.Name}.dll");
+            //            var fullPath = project.Path.GetDirectory().CombineWithFilePath(filePath).MakeAbsolute(_environment);
+            //            arguments.Append($"\"{fullPath}\" --target \"dotnet\" --targetargs \"test -c {settings.Configuration} {project.Path.FullPath} --no-build\" --format opencover --output \"{settings.Output}\"");
+
+//            Run(settings, arguments);
         }
 
         protected override string GetToolName()
@@ -41,12 +57,16 @@ namespace Cake.BCC
 
     public class BCCSubmissionToolSettings : ToolSettings
     {
+        public string InputPath { get; set; }
+        public string Token { get; set; }
+        public string HeadSha { get; set; }
     }
 
     public static class BCCSubmissionToolAliases
     {
         [CakeMethodAlias]
-        public static void BCCSubmission(this ICakeContext context, BCCMSBuildLogToolSettings settings = null)
+        public static void BCCSubmission(this ICakeContext context, string version,
+            BCCMSBuildLogToolSettings settings = null)
         {
             if (context == null)
                 throw new ArgumentNullException(nameof(context));
@@ -54,7 +74,8 @@ namespace Cake.BCC
             if (settings == null)
                 throw new ArgumentNullException(nameof(settings));
 
-            new BCCMSBuildLogTool(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools).BCCMSBuildLog(settings);
+            new BCCMSBuildLogTool(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools)
+                .BCCMSBuildLog(context, version, settings);
         }
     }
 }
