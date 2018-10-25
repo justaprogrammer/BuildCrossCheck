83e04549a249c1756b7f811ea0ec5731726a08d3
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-10-25 10:18:07 -0400

    Attempting fake

--- BCC.Core.IntegrationTests/Utilities/Helper.cs
+++ BCC.Core.IntegrationTests/Utilities/Helper.cs
@@ -5,21 +5,21 @@ namespace BCC.Core.IntegrationTests.Utilities
 {
     public static class Helper
     {
-        public const string GitHubAppPrivateKeyEnvironmentVariable = "_INTEGRATION_GITHUB_KEY";
+        public const string GitHubAppPrivateKeyEnvironmentVariable = "BCC_INTEGRATION_GITHUB_KEY";
 
         public static string GitHubAppPrivateKey => Environment.GetEnvironmentVariable(GitHubAppPrivateKeyEnvironmentVariable);
 
-        public static string GitHubAppIdString => Environment.GetEnvironmentVariable("_INTEGRATION_GITHUB_APPID");
+        public static string GitHubAppIdString => Environment.GetEnvironmentVariable("BCC_INTEGRATION_GITHUB_APPID");
 
-        public static string IntegrationTestAppOwner => Environment.GetEnvironmentVariable("_INTEGRATION_APP_OWNER");
+        public static string IntegrationTestAppOwner => Environment.GetEnvironmentVariable("BCC_INTEGRATION_APP_OWNER");
 
-        public static string IntegrationTestAppRepo => Environment.GetEnvironmentVariable("_INTEGRATION_APP_REPO");
+        public static string IntegrationTestAppRepo => Environment.GetEnvironmentVariable("BCC_INTEGRATION_APP_REPO");
 
-        public static string IntegrationTestAppInstallationId => Environment.GetEnvironmentVariable("_INTEGRATION_APP_INSTALLATION_ID");
+        public static string IntegrationTestAppInstallationId => Environment.GetEnvironmentVariable("BCC_INTEGRATION_APP_INSTALLATION_ID");
 
-        public static string IntegrationTestToken => Environment.GetEnvironmentVariable("_INTEGRATION_TOKEN");
+        public static string IntegrationTestToken => Environment.GetEnvironmentVariable("BCC_INTEGRATION_TOKEN");
 
-        public static string IntegrationTestUsername => Environment.GetEnvironmentVariable("_INTEGRATION_USERNAME");
+        public static string IntegrationTestUsername => Environment.GetEnvironmentVariable("BCC_INTEGRATION_USERNAME");
 
         public static int GitHubAppId => Int32.Parse(GitHubAppIdString);
 
--- Build.fsx
+++ Build.fsx
@@ -1,37 +1,42 @@
 #r "paket:
 nuget Fake.IO.FileSystem
 nuget Fake.DotNet.MSBuild
-nuget Fake.Core.Target //"
+nuget Fake.DotNet.Testing.XUnit2
+nuget Fake.Core.Target
+nuget xunit.runner.console 2.4.0 //"
 #load "./.fake/build.fsx/intellisense.fsx"
 
 open Fake.IO
-open Fake.IO.Globbing.Operators //enables !! and globbing
+open Fake.IO.Globbing.Operators
 open Fake.DotNet
+open Fake.DotNet.Testing.XUnit2
 open Fake.Core
 
-// Properties
-let buildDir = "./build/"
-
-// Targets
 Target.create "Clean" (fun _ ->
-  Shell.cleanDir buildDir
-)
-
-Target.create "BuildApp" (fun _ ->
-  !! "**/*.csproj"
-    |> MSBuild.runRelease id buildDir "Build"
+  !! "BCC.Core.sln"
+    |> MSBuild.run id null "Clean" list.Empty
     |> Trace.logItems "AppBuild-Output: "
 )
 
-Target.create "Default" (fun _ ->
-  Trace.trace "Hello World from FAKE"
+Target.create "Build" (fun _ ->
+  !! "BCC.Core.sln"
+    |> MSBuild.runRelease id null "Build"
+    |> Trace.logItems "AppBuild-Output: "
 )
+
+Target.create "Test" (fun _ ->
+    !! "**/*Tests.dll"
+    |> Fake.DotNet.Testing.XUnit2.run (fun p -> { p with
+                                                    HtmlOutputPath = Some "xunit.html"
+                                                    ToolPath = System.Environment.ExpandEnvironmentVariables("%HOME%/.nuget/packages/xunit.runner.console/2.4.0/tools/net452/xunit.console.exe")
+                                                    })
+)
+
+Target.create "Default" (fun _ -> ())
 
 open Fake.Core.TargetOperators
 
-"Clean"
-  ==> "BuildApp"
-  ==> "Default"
+"Clean" ==> "Build" ==> "Test" ==> "Default"
 
 // start build
 Target.runOrDefault "Default"
