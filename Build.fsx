d754588add48d73d2fcea7ec9ce92477f334b5a7
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-10-26 17:58:14 -0400

    Adding a bit more functionality

--- Build.fsx
+++ Build.fsx
@@ -9,7 +9,16 @@ open Fake.DotNet
 open Fake.DotNet.Testing.XUnit2
 open Fake.Core
 
+BuildServer.install [
+    AppVeyor.Installer
+]
+
+let isAppveyor = AppVeyor.detect()
+
 Target.create "Clean" (fun _ ->
+  !! "reports/**"
+  |> File.deleteAll
+
   let configuration = 
     (fun p -> { p with 
                   Properties = ["Configuration", "Release"]
@@ -29,16 +38,47 @@ Target.create "Build" (fun _ ->
 )
 
 Target.create "Test" (fun _ ->
-    let configuration = (fun p -> { p with HtmlOutputPath = Some "reports/xunit.html" })
+    let configuration = (fun p -> { p with
+                                      HtmlOutputPath = Some "reports/tests.html"
+                                      XmlOutputPath = Some "reports/tests.xml"})
 
     !! "src/**/bin/Release/net471/*Tests.dll"
     |> Fake.DotNet.Testing.XUnit2.run configuration
 )
 
+// coverlet .\BCC.Core.Tests\bin\Release\net471\BCC.Core.Tests.dll 
+// --target "dotnet"
+// --targetargs \"test -c Release .\BCC.Core.Tests\BCC.Core.Tests.csproj --no-build\"
+// --format opencover
+// --output ".\BCC.Core.Tests-net471.coverage.xml"
+
+Target.create "Coverage" (fun _ ->
+    List.allPairs ["BCC.Core.Tests" ; "BCC.Core.IntegrationTests"] ["net471" ; "netcoreapp2.1"]
+    |> Seq.iter (fun (proj, framework) -> 
+            [
+                (sprintf "src\\%s\\bin\\Release\\%s\\%s.dll" proj framework proj) ;
+                "--target \"dotnet\"" ;
+                (sprintf "--targetargs \"test -c Release -f %s src\\%s\\%s.csproj --no-build\"" framework proj proj) ;
+                "--format opencover" ;
+                (sprintf "--output \"./reports/%s-%s.coverage.xml\"" proj framework) ;
+            ]
+            |> String.concat " "
+            |> CreateProcess.fromRawWindowsCommandLine "coverlet"
+            |> Proc.run
+            |> ignore
+        )
+)
+
 Target.create "Default" (fun _ -> ())
 
+let hasCoverlet = true
+
 open Fake.Core.TargetOperators
-"Clean" ==> "Build" ==> "Test" ==> "Default"
+"Clean" ==> "Build"
+"Build" ==> "Test"
+"Build" =?> ("Coverage", hasCoverlet)
+"Test" ==> "Default"
+"Coverage" ==> "Default"
 
 // start build
 Target.runOrDefault "Default"
